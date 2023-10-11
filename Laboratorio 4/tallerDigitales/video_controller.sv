545792a49937a234662b040371a817181cddc9f0
stphnVi <stephanie1117@hotmail.es>
2023-10-11 04:30:41 -0700

    examples (bombs, selected cell, mark cell, grid)

--- Laboratorio 4/tallerDigitales/video_controller.sv
+++ Laboratorio 4/tallerDigitales/video_controller.sv
@@ -36,33 +36,40 @@ module video_controller #(
     horizontal_counter vga_horizontal (clk_25MHz, enable_v_counter, h_count_value);
     vertical_counter vga_vertical (clk_25MHz, enable_v_counter, v_count_value);
 
-    // Sincronización vertical y horizontal basada en estándares VGA
+    // Sincronización vertical y horizontal 
     assign h_synq = (h_count_value < 96) ? 1'b1 : 1'b0;
     assign v_synq = (v_count_value < 2) ? 1'b1 : 1'b0;
 
-    // Asignación de colores a blanco en toda la pantalla
+    
 
     // Zona activa de la pantalla (pantalla en blanco)
     assign blank_n = (h_count_value >= 96) && (h_count_value < 799) && (v_count_value >= 2) && (v_count_value < 524);
     //assign sync_n = 1'b0;
-    //assign blank_n = (h_count_value < 639) && (v_count_value < 479);
+    //assign blank_n = (h_count_value < 799) && (v_count_value < 524);
 
     // Asignar colores a la cuadrícula o al fondo
 
-    logic grid [7:0][7:0] = '{
-        '{1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0},
-        '{1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0},
-        '{1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0},
-        '{1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0},
-        '{1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0},
-        '{1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0},
-        '{1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0},
-        '{1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}
-    };
+logic [1:0] grid [0:7][0:7] = '{
+    '{2'b00,2'b00,2'b00,2'b00,2'b11,2'b00,2'b00,2'b00},
+    '{2'b00,2'b00,2'b11,2'b00,2'b11,2'b00,2'b00,2'b00},
+    '{2'b10,2'b00,2'b11,2'b11,2'b00,2'b00,2'b01,2'b00},
+    '{2'b00,2'b00,2'b00,2'b00,2'b00,2'b00,2'b00,2'b00},
+    '{2'b00,2'b11,2'b00,2'b00,2'b10,2'b00,2'b00,2'b00},
+    '{2'b00,2'b00,2'b00,2'b00,2'b10,2'b00,2'b00,2'b00},
+    '{2'b00,2'b00,2'b00,2'b10,2'b10,2'b00,2'b00,2'b00},
+    '{2'b11,2'b00,2'b00,2'b10,2'b10,2'b00,2'b00,2'b01}
+};
 
     always begin
         draw_game_screen();
     end
+
+	 
+//00 --0 grid (gris y blanco)
+//01 --1 casilla marcada (azul)
+//10 --2 casilla seleccionada (rojo)
+//11 --3 bomba (negro)
+
 
     task draw_game_screen;
         for (int i = 0; i < ROWS; i = i + 1) begin
@@ -82,6 +89,21 @@ module video_controller #(
                                 blue <= 8'b10101010;
                             end
                         end
+								1: begin
+											red <= 8'b00000000;
+											green <= 8'b00000000;
+											blue <= 8'b10101010;
+									end
+								2: begin
+											red <= 8'b11111111;
+											green <= 8'b00000000;
+											blue <= 8'b00000000;
+									end
+								3: begin
+											red <= 8'b00000000;
+											green <= 8'b00000000;
+											blue <= 8'b00000000;
+									end
                     endcase
                 end
             end
