9340162aaa8365dfacab881e98fad8ed623711d4
stphnVi <stephanie1117@hotmail.es>
2023-10-11 02:59:15 -0700

    grid and colors working properly

--- Laboratorio 4/tallerDigitales/video_controller.sv
+++ Laboratorio 4/tallerDigitales/video_controller.sv
@@ -1,25 +1,34 @@
 module video_controller #(
     parameter WIDTH = 640,
-    parameter HEIGHT = 480
+    parameter HEIGHT = 480,
+	 parameter ROWS = 8,
+	parameter COLS = 8
 )
 (
     input logic clock,
     output logic h_synq,
     output logic v_synq,
-    output logic [3:0] red,
-    output logic [3:0] green,
-    output logic [3:0] blue,
+    output logic [7:0] red,
+    output logic [7:0] green,
+    output logic [7:0] blue,
+	 
     output logic clk_25MHz,
     output logic sync_n,
     output logic blank_n
 );
 
     // Dimensiones de las celdas
-    localparam CELL_WIDTH = WIDTH / 8;
-    localparam CELL_HEIGHT = HEIGHT / 8;
+localparam CELL_WIDTH = WIDTH / COLS;
+localparam CELL_HEIGHT = HEIGHT / ROWS;
+localparam BOMB_R = 8'b11111111;
+localparam BOMB_G = 8'b00000000;
+localparam BOMB_B = 8'b00000000;
 
-    // Definir el color blanco (R=15, G=15, B=15)
-    localparam WHITE_COLOR = 4'b1111;
+
+
+
+
+
 
     // VGA variables de control
     logic enable_v_counter;
@@ -41,19 +50,21 @@ module video_controller #(
 
 
 // Zona activa de la pantalla (pantalla en blanco)
-//assign blank_n = (h_count_value >= 96) && (h_count_value < 736) && (v_count_value >= 2) && (v_count_value < 482);
-assign sync_n = 1'b0;
-assign blank_n = (h_count_value < 639) && (v_count_value < 479);
+assign blank_n = (h_count_value >= 96) && (h_count_value < 799) && (v_count_value >= 2) && (v_count_value < 524);
+//assign sync_n = 1'b0;
+//assign blank_n = (h_count_value < 639) && (v_count_value < 479);
 
-	
+/*
 assign red = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? 4'hF: 4'h0;
 assign green = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? 4'hF: 4'h0;
 assign blue = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? 4'hF: 4'h0;
+*/
 
-endmodule
 
-	
-	/*
+	// Asignar colores a la cuadrícula o al fondo
+
+
+
 	
 logic grid [7:0][7:0] = '{
     '{1'b0,1'b0,1'b1,1'b0,1'b0,1'b1,1'b1,1'b0},
@@ -66,40 +77,30 @@ logic grid [7:0][7:0] = '{
     '{1'b0,1'b0,1'b1,1'b0,1'b0,1'b0,1'b1,1'b0}
 };
 
-	
-
-
-	assign sync_n = 1'b0;
-	assign blank_n = h_count_value & v_count_value;
- 
-
-	assign red = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? SNAKE_R: 4'b1111;
-assign green = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? SNAKE_G: 4'b1111;
-assign blue = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? SNAKE_B: 4'b1111;
-endmodule
-
-
-	
 
 	always begin
     draw_game_screen();
-end
-
+	end
 
 	task draw_game_screen;
 		for (int i = 0; i < ROWS; i++) begin
 			for (int j = 0; j < COLS; j++) begin
 				if (v_count_value >= (i * CELL_HEIGHT + 35) && v_count_value < ((i+1) * CELL_HEIGHT + 35) &&
 						h_count_value >= (j * CELL_WIDTH + 144) && h_count_value < ((j+1) * CELL_WIDTH + 144)
-				) begin
-					red <= SNAKE_R;
-						green <= SNAKE_G;
-						blue <= SNAKE_B;
+					) begin
+					if(grid[i][j]) begin
+						red <= BOMB_R;
+						green <= BOMB_G;
+						blue <= BOMB_B;
+					end
+					else begin
+						red <= 8'b00000000;
+						green <= 8'b00000000;
+						blue <= 8'b00000000;	
+					end
 				end
 			end
 		end
 	endtask
-
 endmodule
 
-*/
