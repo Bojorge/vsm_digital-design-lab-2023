497710a70adf5dd0b7144c44811747c967c0cacf
stphnVi <stephanie1117@hotmail.es>
2023-10-11 00:56:49 -0700

    solve problem white squares

--- Laboratorio 4/tallerDigitales/clock_divider.sv
+++ Laboratorio 4/tallerDigitales/clock_divider.sv
@@ -1,24 +1,24 @@
 // clock divider 
 module clock_divider #(parameter div_value = 0) (
-	input logic clk, // 50 MHz
-	output logic divided_clk = 0 // 25 MHz
+    input logic clk, // 50 MHz
+    output logic divided_clk // 25 MHz
 );
 
-	// Division value = 100 MHz / (2 * desired frequency) - 1
-	// Counter
-	integer counter_value = 0; // 32 bit wide bus
+    // Valor de división = 100 MHz / (2 * frecuencia deseada) - 1
+    // Contador
+    integer counter_value = 0;
 
-	always @ (posedge clk)
-	begin
-		// continua contando hasta la division 
-		if (counter_value == div_value)
-		begin
-			counter_value = 0;
-			divided_clk = ~divided_clk;
-		end
-		else
-		begin
-			counter_value = counter_value + 1;
-		end
-	end
+    always_ff @(posedge clk)
+    begin
+        if (counter_value == div_value)
+        begin
+            counter_value = 0;
+            divided_clk = ~divided_clk;
+        end
+        else
+        begin
+            counter_value = counter_value + 1;
+        end
+    end
+
 endmodule
--- Laboratorio 4/tallerDigitales/video_controller.sv
+++ Laboratorio 4/tallerDigitales/video_controller.sv
@@ -1,53 +1,57 @@
-module video_controller 
-#(
-	parameter WIDTH = 640,
-	parameter HEIGHT = 480,
-	parameter ROWS = 8,
-	parameter COLS = 8
+module video_controller #(
+    parameter WIDTH = 640,
+    parameter HEIGHT = 480
 )
 (
-	input logic clock,
-	//input logic [(ROWS-1):0][(COLS-1):0] grid,
-
-	output logic h_synq,
-	output logic v_synq,
-	output logic [3:0] red,
-	output logic [3:0] green,
-	output logic [3:0] blue,
-	output logic clk_25mhz,
-	output logic sync_n,
-	output logic blank_n
+    input logic clock,
+    output logic h_synq,
+    output logic v_synq,
+    output logic [3:0] red,
+    output logic [3:0] green,
+    output logic [3:0] blue,
+    output logic clk_25MHz,
+    output logic sync_n,
+    output logic blank_n
 );
-	/* DIMENSIONES DE LAS CELDAS	 */
-	localparam CELL_WIDTH = WIDTH / COLS;
-	localparam CELL_HEIGHT = HEIGHT / ROWS;
-	
-	/* COLORES */
-localparam SNAKE_R = 4'b0000;
-localparam SNAKE_G = 4'b0000;
-localparam SNAKE_B = 4'b0000;
 
-localparam GRASS_R = 4'b0000;
-localparam GRASS_G = 4'b0000;
-localparam GRASS_B = 4'b0000;
+    // Dimensiones de las celdas
+    localparam CELL_WIDTH = WIDTH / 8;
+    localparam CELL_HEIGHT = HEIGHT / 8;
+
+    // Definir el color blanco (R=15, G=15, B=15)
+    localparam WHITE_COLOR = 4'b1111;
+
+    // VGA variables de control
+    logic enable_v_counter;
+    logic [15:0] h_count_value;
+    logic [15:0] v_count_value;
+
+    // Clock divider
+    clock_divider vga_clock_gen(clock, clk_25MHz);
+
+    // Contadores
+    horizontal_counter vga_horizontal (clk_25MHz, enable_v_counter, h_count_value);
+    vertical_counter vga_vertical (clk_25MHz, enable_v_counter, v_count_value);
+
+    // Sincronización vertical y horizontal basada en estándares VGA
+    assign h_synq = (h_count_value < 96) ? 1'b1 : 1'b0;
+    assign v_synq = (v_count_value < 2) ? 1'b1 : 1'b0;
+
+    // Asignación de colores a blanco en toda la pantalla
 
 
-	
-	// VGA variables de control 
-	logic enable_v_counter;
-	logic [15:0] h_count_value;
-	logic [15:0] v_count_value;
+// Zona activa de la pantalla (pantalla en blanco)
+assign blank_n = (h_count_value >= 96) && (h_count_value < 736) && (v_count_value >= 2) && (v_count_value < 482);
 
-	// Clock divider
-	clock_divider vga_clock_gen(clock, clk_25mhz);
 	
-	// Counters
-	horizontal_counter vga_horizontal (clk_25mhz, enable_v_counter, h_count_value);
-	vertical_counter vga_Vertical (clk_25mhz, enable_v_counter, v_count_value);
+assign red = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? 4'hF: 4'h0;
+assign green = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? 4'hF: 4'h0;
+assign blue = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? 4'hF: 4'h0;
+
+endmodule
+
 	
-	// vertical y horizontal sync basados en los estandares 
-	assign h_synq = (h_count_value < 96) ? 1'b1 : 1'b0;
-	assign v_synq = (v_count_value < 2) ? 1'b1 : 1'b0;
+	/*
 	
 logic grid [7:0][7:0] = '{
     '{1'b0,1'b0,1'b1,1'b0,1'b0,1'b1,1'b1,1'b0},
@@ -66,13 +70,13 @@ logic grid [7:0][7:0] = '{
 	assign sync_n = 1'b0;
 	assign blank_n = h_count_value & v_count_value;
  
-/* 
-	assign red = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? SNAKE_R : GRASS_R;
-assign green = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? SNAKE_G : GRASS_G;
-assign blue = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? SNAKE_B : GRASS_B;
+
+	assign red = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? SNAKE_R: 4'b1111;
+assign green = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? SNAKE_G: 4'b1111;
+assign blue = (h_count_value < 784 && h_count_value > 143 && v_count_value < 515 && v_count_value > 34) ? SNAKE_B: 4'b1111;
 endmodule
 
-*/
+
 	
 
 	always begin
@@ -95,3 +99,5 @@ end
 	endtask
 
 endmodule
+
+*/
