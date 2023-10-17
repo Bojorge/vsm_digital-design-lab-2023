f9f030697bf588d02eeef7353a5b297b3807deb8
Bojtronic <bojorgearayamanuel@gmail.com>
2023-10-17 12:27:31 -0600

    Control del grid con los switches

--- Laboratorio 4/Buscaminas/TopModule.sv
+++ Laboratorio 4/Buscaminas/TopModule.sv
@@ -1,65 +1,25 @@
-module TopModule 
-(
-	 input logic clk,
-    input logic rst, 
-    output logic h_synq,
-    output logic v_synq,
-    output logic [7:0] red,
-    output logic [7:0] green,
-    output logic [7:0] blue,
-	 output logic sync_n,
-    output logic blank_n,
-    output logic clk_25MHz
-    
-);
-
-
-	
-	logic T;
-	logic C1;
-	logic A; 
-	logic C2;
-	logic x;
-	
-	logic [2:0] grid [0:7][0:7];
-	
-		
-	generateGrid gen_grid( 
-			.grid(grid) 
-	); 
+module TopModule(input logic clk,
+			  input logic [2:0] i,
+			  input logic [2:0] j,
+			  input logic select,
+			  output logic vga_clk,
+			  output logic h_sync, v_sync,
+			  output logic sync_b, blank_b, // To monitor & DAC
+			  output logic [7:0] red, green, blue); // To video DAC
+			  
+	logic [9:0] x, y;
+	logic [9:0] i_aux;
+	logic [9:0] j_aux;
 	
 	
-	 
-	clock_divider vga_clock_gen(
-			.clk(clk), 
-			.divided_clk(clk_25MHz)
-	);
+	assign i_aux = 80 + i*60;
+	assign j_aux = j*60;
 	
+	pll vga_pll(.clk(clk), .vga_clk(vga_clk));
 	
-	FiniteStateMachine fsm (
-			.clk(clk), 
-			.rst(rst), 
-			.T(T), 
-			.C1(C1), 
-			.A(A), 
-			.C2(C2), 
-			.x(x)
-	);
+	// Generate monitor timing signals
+	vga_controller vgaCont(vga_clk, h_sync, v_sync, sync_b, blank_b, x, y);
 	
-
-	
-	grid_controller #(640, 480, 8, 8) grid_controller_inst (
-		  .grid(grid),
-		  .clk_25MHz(clk_25MHz),
-        .h_synq(h_synq),
-        .v_synq(v_synq),
-        .red(red),
-        .green(green),
-        .blue(blue),
-        .sync_n(sync_n),
-        .blank_n(blank_n)
-    );
-	
-	
-
+	// User-defined module to determine pixel color
+	generate_grid gen_grid(x, y, i_aux, j_aux, select, red, green, blue);
 endmodule
